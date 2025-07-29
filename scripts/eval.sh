324694db2d89d1f87e544f12fc6f82f9b08f823f
danieljyh <danieljyh@snu.ac.kr>
2025-07-29 10:53:23 +0900

    cmt

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,28 +1,17 @@
 export TRITON_LIBCUDA_PATH="/tools/cuda/cuda12.1/targets/x86_64-linux/lib/stubs"
 
 # Model: llava_ov_0.5b llava_ov_7b llava_ov_72b video_llava_7b longva_7b
-model="llava_ov_7b"
+model="llava_ov_0.5b"
 method="rekv"
 # method="sampling"
 
 # Dataset: qaego4d egoschema cgbench mlvu activitynet_qa rvs_ego rvs_movie
-tasks=(qaego4d)
+# tasks=(qaego4d)
+tasks=(mlvu)
 
 fps=0.5
-retrieve_sizes=(16 32 128)
+retrieve_sizes=(64)
 
-for retrieve_size in ${retrieve_sizes[@]}; do
-    python -m main.eval \
-        --model $model \
-        --method $method \
-        --tasks $tasks \
-        --sample_fps $fps \
-        --retrieve_size $retrieve_size \
-        --postfix $retrieve_size
-done
-
-
-model="llava_ov_0.5b"
 for retrieve_size in ${retrieve_sizes[@]}; do
     python -m main.eval \
         --model $model \
